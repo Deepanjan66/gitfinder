3fe08b9dc7894bb8ebb101895f652397ca089c73
Deepanjan <deepanjan.chakrabarty@student.unsw.edu.au>
2018-03-22 23:45:51 +1100

    Feat: Added documentation for the shell script

--- gitfinder.sh
+++ gitfinder.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Variables that hold values obtained from the
+# arg flags
 commit='';
 folder=0;
+
+# Store current directory
 curr_dir=`pwd`;
 
-
+# Function used for printing error statements
 show_errors()
 {
    if [ $? -ne 0 ];
@@ -14,9 +18,10 @@ show_errors()
    fi
 }
 
-
+# For all arguments in the arg list
 while [[ $# -gt 0 ]];
 do
+   # Update variable values depending on the flags
    if [ "$1" = "-f" ];
    then
       folder=1;
@@ -25,6 +30,7 @@ do
    then
       commit="$2";
       shift 2;
+   # Show -h output for any other flags provided
    else
       echo "Usage: gitfinder <flags> <values>"
       echo "Flags:"
@@ -35,19 +41,25 @@ do
    fi
 done
 
+# Array that will store all the repositories that are found
 results=();
-
+# Find all the folders that have a .git file or folder
 repos=( $(find . 2> /dev/null | egrep -E "\.git$") );
 
+# For every folder that has .git
 for ((i=0; i<${#repos[@]}; i++ ));
 do
+   # Remove the .git at the end
    dirname="${repos[$i]}";
    dirname=`echo "$dirname" | sed s/\.git$//g 2> /dev/null`;
    show_errors "Encountered an error while looking at ${repos[$i]}";
+   # If dirname is not a directory, move to the next repository dir
    if [ ! -d "$dirname" ];
    then
       continue;
    fi
+   # If commit flag is provided, look for required commit message
+   # from the commit log
    if [[ $commit ]];
    then
       cd $dirname;
@@ -63,10 +75,12 @@ do
       fi
       cd $curr_dir;
    else
+      # Add all results if no filters are required
       results+=(${repos[i]}); 
    fi
 done
 
+# Print all the results
 echo "===================== RESULTS ========================"
 for ((i=0; i<${#results[@]}; i++ ));
 do
